7d03f9f510f937bbed4d59415ceac7ad453d93d3
haurenburu <haurenburu@protonmail.com>
2019-11-09 17:30:57 -0300

    next is now working

--- src/App.js
+++ src/App.js
@@ -1,7 +1,6 @@
 import React from 'react';
 import Pokemon from './components/Pokemon';
 
-
 function App() {
   return (
     <div className="App">
--- src/components/Pokemon.js
+++ src/components/Pokemon.js
@@ -2,6 +2,7 @@ import React from 'react';
 import Type from './Type';
 
 export default class Pokemon extends React.Component {
+
     constructor() {
         super();
         this.state = {
@@ -13,8 +14,32 @@ export default class Pokemon extends React.Component {
             next: 'https://pokeapi.co/api/v2/pokemon/1',
             height: null,
             weight: null,
-            sprite: null
+            sprite: null,
         };
+    }
+    async componentNext() {
+        const url = this.state.next;
+        const res = await fetch(url);
+        const pokemon = await res.json();
+        
+
+        this.setState({
+            id: pokemon.id,
+            name: pokemon.name,
+            types: pokemon.types.length,
+            type1: pokemon.types[0].type.name,
+            type2: null,
+            next: 'https://pokeapi.co/api/v2/pokemon/'+(pokemon.id+1),
+            height: pokemon.height,
+            weight: pokemon.weight,
+            sprite: pokemon.sprites.front_default
+        });
+
+        if(pokemon.types.length === 2){
+            this.setState({
+                type2: pokemon.types[1].type.name,
+            })
+        }
     }
     async componentDidMount() {
         const url = this.state.next;
@@ -39,24 +64,23 @@ export default class Pokemon extends React.Component {
                 type2: pokemon.types[1].type.name,
             })
         }
-        
-        
 
-        
     }
     render() {
         return (
             <div>
-                <img src={this.state.sprite} ></img>
+                <img src={this.state.sprite} alt="pokemon"></img>
                 <div>id: {this.state.id}</div>
                 <div>name: {this.state.name}</div>
+
                 <Type type={this.state.type1} />
-                {this.state.types == 2 ? <Type type={this.state.type2} /> : null}
+                {this.state.types === 2 ? <Type type={this.state.type2} /> : null}
                 <div>height: {this.state.height}</div>
                 <div>weight: {this.state.weight}</div>
                 <div>next: {this.state.next}</div>
+                <p>{this.props.poke}</p>
+                <button onClick={() => this.componentNext()}>Next</button>
                 {/* <img src={"https://assets.pokemon.com/assets/cms2/img/pokedex/detail/0" + this.state.id +".png"}></img> */}
-                
             </div>
         );
     }
